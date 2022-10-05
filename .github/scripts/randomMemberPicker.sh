b2fb38749cf4b9115844126f0d830aa2674a82a5
Pip <94843495+pipclark@users.noreply.github.com>
2022-10-05 15:12:16 +0200

    include atmospheric noise random number generator

--- .github/scripts/randomMemberPicker.sh
+++ .github/scripts/randomMemberPicker.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 # read list of current team members
-filename="`dirname $0`"/'teamMembers.txt'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+membersfile="${DIR}/data/berlinTeamMembers.txt"
 n=1
 while read -r line; do
 # reading each line
 MEMBERS[$n]=$line
 n=$((n+1))
-done < $filename
+done < $membersfile
 
-# shuffle array
-MEMBERS=( $(shuf -e "${MEMBERS[@]}") )
+# pull random integer between 1 and # of team members from atmospheric noise random number generator to make Will happier
+i=$(curl "https://www.random.org/integers/?format=plain&num=1&min=1&max=${#MEMBERS[@]}&col=1&base=10&frnd=new")
+res=$?
+# falls back to shell random generator in case curl fails (res not 0)
+if test "$res" != "0" 
+then
+   echo "the curl command failed with: $res"
+   i=$(shuf -i 1-${#MEMBERS[@]} -n 1)
+fi
 
-echo "::set-output name=FACILITATOR::${MEMBERS[0]}"
+echo "::set-output name=FACILITATOR::${MEMBERS[$i]}"
